9b62dc6c7856136fabb6185ff5b547cf3db405db
Gabriel Augusto <gabrielaugustosergio@gmail.com>
2022-06-03 22:14:18 -0300

    CRUD Scaffolding

---
+++ SistemaWebMvc/Data/SistemaWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaWebMvc.Models;
+
+namespace SistemaWebMvc.Data
+{
+    public class SistemaWebMvcContext : DbContext
+    {
+        public SistemaWebMvcContext (DbContextOptions<SistemaWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaWebMvc.Models.Department> Department { get; set; }
+    }
+}
